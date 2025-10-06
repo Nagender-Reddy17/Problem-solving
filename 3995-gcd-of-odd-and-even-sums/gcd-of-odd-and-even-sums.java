f7b7ec222ef7b56df54e8e09818e09b7eba3f371
Nagender-Reddy17 <nagenderreddy1707@gmail.com>
2025-10-06 20:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (26.08%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,22 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int osum=0,esum=0,max=1;
-        for(int i=1;i<=n+n;i++){
-             if(i%2==0){
-                esum+=i;
-             }
-             else{
-                osum+=i;
-             }
-        }
-             int m=Math.min(esum,osum);
-
-             for(int i=1;i<=m;i++){
-                if(esum%i==0 && osum%i==0 ){
-                    max=i;
-                }
-             }
-        
-        return max;
+       return n;
     }
 }
